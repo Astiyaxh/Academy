1e877608924a330d8fbff38639c89e0033b818bb
DjangoRepoMngr <98884546+DjangoRepoMngr@users.noreply.github.com>
2022-10-03 13:46:21 +0330

    configured statics and media urls

--- src/academy/settings.py
+++ src/academy/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -101,6 +102,18 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+STATIC_URL = '/static/'
+if DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static-files')
+else:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+]
+LOGIN_URL = "/login"
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
--- src/academy/urls.py
+++ src/academy/urls.py
@@ -13,8 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.urls import include, path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
 from user.views import (
     profile
 )
@@ -23,3 +26,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('profile/', profile, name='profile'),
 ]
+
+
+urlpatterns += staticfiles_urlpatterns()
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
