5ad7850e5417f367ba7546eaa62bac0121551b2c
DjangoRepoMngr <98884546+DjangoRepoMngr@users.noreply.github.com>
2022-10-12 17:02:05 +0330

    finished working on institude english registor

--- src/education/views.py
+++ src/education/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponseRedirect
+from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from education import models,forms
 
@@ -13,10 +14,21 @@ def english_institude_register(request):
     })
 
     if request.method == "POST":
+        term_ids = request.POST.getlist('TermId')
+        education_contents = request.POST.getlist('education_content')
+        session_numbers = request.POST.getlist('session_number')
+        max_students = request.POST.getlist('max_student')
+        session_prices = request.POST.getlist('session_price')
+        nsession_prices = request.POST.getlist('nsession_price')
+
         if form.is_valid():
             obj = form.save()
             obj.user = request.user
             obj.save()
+            if len(term_ids) > 0:
+                for term_id,education_content,session_number,max_student,session_price,nsession_price in zip(term_ids,education_contents,session_numbers,max_students,session_prices,nsession_prices):
+                    models.EnglishInstituteForm.objects.create(english_institute_register = obj , english_institute_define_term = models.EnglishInstituteDefineTerm.objects.get(id = term_id ),education_content = education_content, session_number = session_number,max_student = max_student, session_price = session_price,nsession_price = nsession_price )
+            return HttpResponseRedirect(reverse('education:froms_list'))
 
     return render(request, 'education/english_institude_register.html', context)
 
