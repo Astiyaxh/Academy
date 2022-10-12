95b8f4d17bc380dad0795a58f4c8a8a24b3d34c4
Astiyaxh <ariyan.mehri00@gmail.com>
2022-10-12 16:31:31 +0330

    delete English Institute forms

--- src/education/views.py
+++ src/education/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from education import models,forms
+from django.urls import reverse
 
 @login_required
 def english_institude_register(request):
@@ -35,12 +36,23 @@ def english_institude_register(request):
     return render(request, 'education/english_institude_register.html', context)
 
 
-def forms_list(request):
+def english_institute_forms_list(request):
    english_institutes_registers = models.EnglishInstituteRegister.objects.filter(user=request.user)
-
    context = {
     'english_institutes_registers' : english_institutes_registers
    }
-
    return render(request, 'education/forms_list.html', context)
+
+
+def delete_english_institute_forms_list(request,id):
+    obj = get_object_or_404(models.EnglishInstituteRegister, id = id)
+    try:
+        obj.delete()
+    except:
+        obj.status = -1
+        obj.save()
+
+    return HttpResponseRedirect(reverse("education:english_institute_forms_list"))
+ 
+
         
