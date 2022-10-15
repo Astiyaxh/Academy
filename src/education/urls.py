ed6a08d07eb109a65c0e82be9b177f40935d54d7
DjangoRepoMngr <98884546+DjangoRepoMngr@users.noreply.github.com>
2022-10-15 13:40:15 +0330

    finsished 80% of working on edit view english institute register

--- src/education/urls.py
+++ src/education/urls.py
@@ -4,6 +4,7 @@ from education import views
 app_name = 'education'
 urlpatterns = [
     path('english_institude_register/', views.english_institude_register, name='english_institude_register'),
+    path('english_institude_edit/<int:id>', views.english_institude_edit, name='english_institude_edit'),
     path('english_institute_forms_list/', views.english_institute_forms_list, name='english_institute_forms_list'),
     path('delete_english_institute_forms_list/<int:id>', views.delete_english_institute_forms_list, name='delete_english_institute_forms_list'),
 ]
