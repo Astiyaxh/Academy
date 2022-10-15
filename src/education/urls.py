709c60572824590f6ea1238139287056f073ba9d
Astiyaxh <ariyan.mehri00@gmail.com>
2022-10-15 13:40:21 +0330

    Create send_english_institute_forms_list, template, url

--- src/education/urls.py
+++ src/education/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('english_institude_register/', views.english_institude_register, name='english_institude_register'),
     path('english_institute_forms_list/', views.english_institute_forms_list, name='english_institute_forms_list'),
     path('delete_english_institute_forms_list/<int:id>', views.delete_english_institute_forms_list, name='delete_english_institute_forms_list'),
+    path('send_english_institute_forms_list/<int:id>', views.send_english_institute_forms_list, name='send_english_institute_forms_list'),
 ]
